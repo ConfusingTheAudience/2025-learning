bd55c25e76e6256b841700bac35ca6a2babaa2e8
ConfusingTheAudience <testingmailpurpos@example.com>
2025-11-13 08:42:25 +0100

    lesson36

---
+++ 03 - python/lesson36.py
@@ -0,0 +1,23 @@
+# PyQt5
+
+# 61. PyQt5 GUI Introduction
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+# from PyQt5.QtGui import QIcon = for icon
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('My cool first GUI')
+        self.setGeometry(700, 300, 500, 500)
+      # self.setWindowIcon(QIcon('profile_pic.jpg')) = for icon
+
+def main():
+    app = QApplication(sys.argv) # sys.argv or empty list
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
