ea98882406d182ab219fe84c1540f9b380405907
ConfusingTheAudience <testingmailpurpos@example.com>
2025-11-17 16:42:50 +0100

    python flask forms&post*files

---
+++ 04 - flask/A04-form&post&files/app.py
@@ -0,0 +1,115 @@
+import os
+import uuid
+import pandas as pd
+from flask import Flask, render_template, request, Response, send_from_directory, jsonify
+
+app = Flask(__name__, template_folder='templates')
+
+# --------------------------------------------------------------------
+# FORM REQUEST HANDLE WITH POST
+# --------------------------------------------------------------------
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        # username = request.form['username']
+        # or
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == 'accepted' and password == "y":
+            return 'Success'
+        else:
+            return 'Failure'
+# --------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------
+# FILE UPLOAD HANDLE WITH POST
+# --------------------------------------------------------------------
+
+# in html name is important in forms
+@app.route('/file_upload', methods=['POST'])
+def file_upload():
+    file = request.files['file']
+
+    if file.content_type == 'text/plain':
+        return file.read().decode()
+    elif file.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' or 'application/vnd.ms-excel':
+        df = pd.read_excel(file)
+        return df.to_html()
+# --------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------
+# RETURN A FILE FROM DOWNLOAD
+# --------------------------------------------------------------------
+
+# install and import panda
+# import Response
+@app.route('/convert_csv', methods=['POST'])
+def convert_csv():
+    file = request.files['file']
+
+    df = pd.read_excel(file)
+
+    response = Response(
+        df.to_csv(),
+        mimetype='text/csv',
+        headers={
+            'Content-Disposition': 'attachment; filename=result.csv'
+        }
+    )
+    return response
+# --------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------
+# RETURN A FILE 2 (WITH DOWNLOAD TEMPLATE)
+# --------------------------------------------------------------------
+
+# we need import os
+# we need import uuid
+# we need import send_from_directory
+@app.route('/convert_csv_two', methods=['POST'])
+def convert_csv_two():
+    file = request.files['file']
+
+    df = pd.read_excel(file)
+
+    if not os.path.exists('downloads'):
+        os.makedirs('downloads')
+
+    filename = f'{uuid.uuid()}.csv'
+    df.to_csv(os.path.join('downloads', filename))
+
+    return render_template(template_name_or_list='download.html', filename=filename)
+
+@app.route('/download/<filename>')
+def download(filename):
+    return send_from_directory(directory='downloads', filename=filename, download_name='result.csv')
+# --------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------
+# POST JSON DATA WITH JAVASCRIPT
+# --------------------------------------------------------------------
+
+# import jsonify
+# here id in html button is important because we working on it to communicate with backend
+@app.route('/handle_post', methods=['POST'])
+def handle_post():
+    greeting = request.json['greeting']
+    name = request.json['name']
+
+    with open('file.txt', 'w') as f:
+        f.write(f'{greeting}, {name}')
+    
+    return jsonify({'message': 'Successfully written!'})
+# --------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
